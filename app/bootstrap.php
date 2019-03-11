c437f219880c849668803c81ced8d6833440680e
Jakub Bouček <pan@jakubboucek.cz>
2019-03-11 04:13:16 +0100

    Debug mode
    - return default behavion
    - add report mail to me

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-$configurator->setDebugMode('185.153.194.166'); // enable for your remote IP
-$configurator->enableTracy(__DIR__ . '/../log');
+//$configurator->setDebugMode('185.153.194.166'); // enable for your remote IP
+$configurator->enableTracy(__DIR__ . '/../log', 'pan@jakubboucek.cz');
 
 $configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
